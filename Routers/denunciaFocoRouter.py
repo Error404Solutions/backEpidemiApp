5b98d7ba04b205644379953ff964b78e53923ae5
AlejandroLealGonzalez <alejandro.leal05@outlook.com>
2024-06-08 15:57:34 -0500

    Agregado endPoint consultar por nombre de ubicacion

--- Routers/denunciaFocoRouter.py
+++ Routers/denunciaFocoRouter.py
@@ -1,10 +1,9 @@
 from typing import List
 from pydantic import BaseModel
 from sqlalchemy import func, select
-from sqlalchemy.orm import sessionmaker
 from Schema.denunciaFocoSchema import denunciaFocoSchema
 from Config.db import engine
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, HTTPException, status, Response
 from Models.denunciaFocoModel import denunciaFocoM
 
 
@@ -31,7 +30,7 @@ async def listarDenunciasF():
         return denunciaFList
 
 
-# Definimos el esquema de respuesta
+# Definimos el esquema de respuesta para el conteo de focos por ubicacion
 class DenunciaFocoCountSchema(BaseModel):
     ubicacion: str
     count: int
@@ -50,3 +49,16 @@ async def listarDenunciasUbic():
         denunciaFList = [{"ubicacion": row[0], "count": row[1]} for row in result]
         return denunciaFList
 
+
+@denunciaF.get("/api/denunciaF/{ubicacion_name}", response_model=List[denunciaFocoSchema])
+async def obtenerFocosUbicacion(ubicacion_name: str):
+    with engine.connect() as conn:
+        query = select(denunciaFocoM).where(denunciaFocoM.c.ubicacion == ubicacion_name)
+        result = conn.execute(query).fetchall()
+        if not result:
+            raise HTTPException(status_code=404, detail="Ubicacion no encontrada")
+        
+        # Convertir el resultado en una lista de diccionarios
+        ubicList = [dict(row._mapping) for row in result]
+        return ubicList
+        
