756eeea08811bd63af4a7554c5f0eddd3387efd9
AlejandroLealGonzalez <alejandro.leal05@outlook.com>
2024-06-08 15:27:45 -0500

    Agregado endpoint para listar las ubicaciones con su cantidad de focos

--- Routers/denunciaFocoRouter.py
+++ Routers/denunciaFocoRouter.py
@@ -1,8 +1,14 @@
 from typing import List
+from pydantic import BaseModel
+from sqlalchemy import func, select
+from sqlalchemy.orm import sessionmaker
 from Schema.denunciaFocoSchema import denunciaFocoSchema
 from Config.db import engine
 from fastapi import APIRouter, status, Response
 from Models.denunciaFocoModel import denunciaFocoM
+
+
+
 
 denunciaF = APIRouter()
 
@@ -23,3 +29,24 @@ async def listarDenunciasF():
         #convirtiendo el resultado en una lista de diccionarios
         denunciaFList = [dict(row._asdict()) for row in result]
         return denunciaFList
+
+
+# Definimos el esquema de respuesta
+class DenunciaFocoCountSchema(BaseModel):
+    ubicacion: str
+    count: int
+
+@denunciaF.get("/api/denunciaFocoUbicacion", response_model=List[DenunciaFocoCountSchema])
+async def listarDenunciasUbic():
+    with engine.connect() as conn:
+        # Realizar la consulta con GROUP BY y COUNT
+        query = (
+            select(denunciaFocoM.c.ubicacion, func.count(denunciaFocoM.c.ubicacion).label('count'))
+            .group_by(denunciaFocoM.c.ubicacion)
+        )
+        result = conn.execute(query).fetchall()
+        
+        # Convirtiendo el resultado en una lista de diccionarios
+        denunciaFList = [{"ubicacion": row[0], "count": row[1]} for row in result]
+        return denunciaFList
+
